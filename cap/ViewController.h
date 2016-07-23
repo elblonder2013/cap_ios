17059c5be0a744466ed723325df1844050f7dbfd
Alexei Pineda Caraballo <alexei@alexeis-mac.local>
2016-07-22 22:27:46 -0400

    Initial Commit

---
+++ cap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cap
+//
+//  Created by Alexei Pineda Caraballo on 7/22/16.
+//  Copyright © 2016 Alexei Pineda Caraballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  cap
+//
+//  Created by Alexei Pineda Caraballo on 7/22/16.
+//  Copyright © 2016 Alexei Pineda Caraballo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
